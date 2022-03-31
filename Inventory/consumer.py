73985bab0aae8d6298e46954dbdd33851297c0f1
sujeongHeo401 <gotnwjd40@gmail.com>
2022-04-01 02:20:55 +0900

    first commit

---
+++ Inventory/consumer.py
@@ -0,0 +1,23 @@
+from main import redis, Product
+import time
+
+key = 'order_completed'
+group = 'inventory-group'
+try:
+    redis.xgroup_create(key, group)
+except:
+    print('Group already exists!')
+
+
+while True:
+    try:
+        results = redis.xreadgroup(group, key, {key: '>'}, None)
+        print(results)
+
+    except Exception as e:
+        print(str(e))
+    time.sleep(1)
+    
+
+
+///////// https://youtu.be/Cy9fAvsXGZA
